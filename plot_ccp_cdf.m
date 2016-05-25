65b3f4fb26d2c9cc19c4d06fcef73f7e003b180c
Zach Weinberg <weinberz@gmail.com>
2016-05-25 18:33:49 -0400

    Added normalization for cell size to plot_ccp_cdf, but keeping off by default (for now)

--- plot_ccp_cdf.m
+++ plot_ccp_cdf.m
@@ -20,18 +20,29 @@ p = inputParser;
 p.addOptional('Color', 'b');
 p.addOptional('Graph', {});
 p.addOptional('Floor', 0, @isscalar);
+p.addOptional('Normalize',false, @isboolean);
 p.parse(varargin{:});
 
 graphColor = p.Results.Color;
 graphAxis = p.Results.Graph;
 edfFloor = p.Results.Floor;
+isNormalized = p.Results.Normalize;
+
 
 % find the floor
 t_res = resStruct.lftRes.t;
 startIdx = find(t_res>=edfFloor,1);
 t_res = resStruct.lftRes.t(startIdx:end);
 
-edf_res = sum(resStruct.lftRes.lftHist_total(:,startIdx:end),1);
+lftHist_total = resStruct.lftRes.lftHist_total;
+
+% normalize to cell area
+if isNormalized
+  cellArea = resStruct.lftRes.cellArea;
+  lftHist_total = bsxfun(@rdivide, lftHist_total, cellArea);
+end
+
+edf_res = sum(lftHist_total(:,startIdx:end),1);
 epdf_res = edf_res/sum(edf_res);
 ecdf_res = cumsum(epdf_res);
 plot(t_res, ecdf_res, graphColor);
